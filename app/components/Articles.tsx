2810b3db6f8dd1d03577f8f82b8da936940a2a26
TheDrifter408 <nurullahan408@gmail.com>
2024-10-16 15:52:17 +0600

    changed sections styling

--- app/components/Articles.tsx
+++ app/components/Articles.tsx
@@ -3,7 +3,7 @@ import ArticleItem from "./ArticleItem";
 
 export default function Articles({ data }:ArticleProps) {
     return(
-        <section className="mx-auto">
+        <section className="mx-auto border border-black">
             {
                 data?.map((article:ArticleItemProps,idx:number) => (
                     <ArticleItem key={idx} {...article} />
