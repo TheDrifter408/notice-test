49bfb42970c937cfcf4035215ce702e3f3ea53c1
TheDrifter408 <nurullahan408@gmail.com>
2024-10-16 15:46:20 +0600

    paragraph styling

--- app/components/ArticleItem.tsx
+++ app/components/ArticleItem.tsx
@@ -4,9 +4,9 @@ import { ArticleItemProps } from "../types";
 export default function ArticleItem({id,title,paragraph}:ArticleItemProps){
     return(
         <div className="w-[90%] rounded-lg">
-            <h2 className="capitalize">{title}</h2>
+            <h2 className="capitalize text-xl">{title}</h2>
             <article>
-                {paragraph}
+                <p className="text-pretty my-2">{paragraph}</p>
             </article>
         </div>
     )
